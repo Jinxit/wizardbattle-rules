509c838d30aba506ccd46b1aa3b96af2680e648e
Lucas Åström <lucasas@kth.se>
2014-10-30 10:34:29 +0100

    added utils which contains a way to easily generate a valid starter board

---
+++ src/utils.hpp
@@ -0,0 +1,61 @@
+#ifndef WIZARDBATTLE_RULES_UTILS_H
+#define WIZARDBATTLE_RULES_UTILS_H
+
+#include <vector>
+#include <algorithm>
+
+#include "board.hpp"
+#include "wizard.hpp"
+
+namespace WizardBattle
+{
+	namespace Rules
+	{
+		namespace
+		{
+			unsigned int unifRandInt(unsigned int min, unsigned int max)
+			{
+				return min + (rand() % (unsigned int)(max - min + 1));
+			}
+		}
+
+		inline Board createBoard(unsigned int width, unsigned int height, unsigned int numPlayers)
+		{
+			std::vector<Wizard> wizards;
+			auto it = wizards.begin();
+
+			switch (numPlayers)
+			{
+				case 4:
+					wizards.emplace(it, width - 1, 0);
+				case 3:
+					it = wizards.begin();
+					wizards.emplace(it, 0, height - 1);
+				case 2:
+					it = wizards.begin();
+					wizards.emplace(it, width - 1, height - 1);
+				case 1:
+					it = wizards.begin();
+					wizards.emplace(it, 0, 0);
+			}
+
+
+			std::vector<unsigned int> temperatures;
+			for (unsigned int y = 0; y < height; y++)
+			{
+				for (unsigned int x = 0; x < width; x++)
+				{
+					unsigned int limit = std::max(
+						std::max(x, width - x - 1),
+						std::max(y, height - y - 1)
+					);
+					temperatures.emplace_back(unifRandInt(1, limit));
+				}
+			}
+
+			return Board(width, height, wizards, temperatures, PHASE_MOVE);
+		}
+	}
+}
+
+#endif

c9ee8b2083d165e540289291315a1e9d33b5b526
Lucas Åström <lucasas@kth.se>
2014-10-30 09:19:25 +0100

    implemented getPossibleMoves and utilities getPosition and isWizardAt

--- src/board.hpp
+++ src/board.hpp
@@ -24,14 +24,89 @@ namespace WizardBattle
 			Phase phase;
 
 			Board(unsigned int width, unsigned int height,
-				std::vector<Wizard> wizards, std::vector<unsigned int> temperatures)
-				: width(width), height(height), wizards(wizards), temperatures(temperatures) { };
+				std::vector<Wizard> wizards, std::vector<unsigned int> temperatures, Phase phase)
+				:	width(width), height(height), wizards(wizards), temperatures(temperatures),
+					phase(phase) { };
 
-			std::vector<Move> getPossibleMoves(unsigned int wizardID) const;
+			std::vector<Move> getPossibleMoves(unsigned int wizardID) const
+			{
+				std::vector<Move> moves;
+
+				if (phase == PHASE_TEMPERATURE)
+				{
+					for (std::vector<unsigned int>::size_type i = 0; i < temperatures.size(); i++)
+					{
+						unsigned int x = -1, y = -1;
+						std::tie(x, y) = getPosition(i);
+
+						if (!isWizardAt(x, y))
+						{
+							moves.emplace_back(ACTION_RAISE, x, y);
+							moves.emplace_back(ACTION_LOWER, x, y);
+						}
+					}
+				}
+				else if (phase == PHASE_MOVE)
+				{
+					auto & wiz = wizards[wizardID];
+					auto & temp = temperatures[getIndex(wiz.x, wiz.y)];
+
+					if (wiz.x + temp < width)
+					{
+						if (!isWizardAt(wiz.x + temp, wiz.y))
+						{
+							moves.emplace_back(ACTION_MOVE, wiz.x + temp, wiz.y);
+						}
+					}
+					if (temp <= wiz.x)
+					{
+						if (!isWizardAt(wiz.x - temp, wiz.y))
+						{
+							moves.emplace_back(ACTION_MOVE, wiz.x - temp, wiz.y);
+						}
+					}
+					if (wiz.y + temp < height)
+					{
+						if (!isWizardAt(wiz.x, wiz.y + temp))
+						{
+							moves.emplace_back(ACTION_MOVE, wiz.x, wiz.y + temp);
+						}
+					}
+					if (temp <= wiz.y)
+					{
+						if (!isWizardAt(wiz.x, wiz.y - temp))
+						{
+							moves.emplace_back(ACTION_MOVE, wiz.x, wiz.y - temp);
+						}
+					}
+				}
+
+				return moves;
+			};
+
 			std::vector<Board> getPossibleNextStates(unsigned int wizardID) const;
 			unsigned int getIndex(unsigned int x, unsigned int y) const
 			{
 				return x + y * width;
+			};
+
+			std::tuple<unsigned int, unsigned int> getPosition(unsigned int index) const
+			{
+				return std::tuple<unsigned int, unsigned int>(index % width, index / width);
+			};
+
+			bool isWizardAt(unsigned int x, unsigned int y) const
+			{
+				bool found = false;
+				for (std::vector<Wizard>::size_type i = 0; i < wizards.size(); i++)
+				{
+					if (wizards[i].x == x && wizards[i].y == y)
+					{
+						found = true;
+						break;
+					}
+				}
+				return found;
 			};
 		};
 	}

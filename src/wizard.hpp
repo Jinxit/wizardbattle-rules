18f368de73edc0c1f80a8812fea69813046eeb2c
Lucas Åström <lucasas@kth.se>
2014-10-30 07:40:49 +0100

    shuffled wizard constructor a bit, added definition for kill

--- src/wizard.hpp
+++ src/wizard.hpp
@@ -8,14 +8,19 @@ namespace WizardBattle
 		class Wizard
 		{
 		private:
+			Wizard(unsigned int x, unsigned int y, bool alive)
+				: x(x), y(y), alive(alive) { };
 		public:
 			const unsigned int x, y;
 			const bool alive;
 
 			Wizard(unsigned int x, unsigned int y)
-				: x(x), y(y), alive(false) { };
+				: Wizard(x, y, true) { };
 
-			const Wizard kill() const;
+			const Wizard kill() const
+			{
+				return Wizard(x, y, false);
+			};
 		};
 	}
 }

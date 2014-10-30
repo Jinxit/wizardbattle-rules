294013e592221adb07ec163e998943b4971a2a37
Lucas Åström <lucasas@kth.se>
2014-10-30 07:26:44 +0100

    added members to board

--- src/board.hpp
+++ src/board.hpp
@@ -1,6 +1,9 @@
 #ifndef WIZARDBATTLE_RULES_BOARD_H
 #define WIZARDBATTLE_RULES_BOARD_H
 
+#include <vector>
+#include "wizard.hpp"
+
 namespace WizardBattle
 {
 	namespace Rules
@@ -9,6 +12,13 @@ namespace WizardBattle
 		{
 		private:
 		public:
+			const unsigned int width, height;
+			const std::vector<Wizard> wizards;
+			const std::vector<unsigned int> temperatures;
+
+			Board(unsigned int width, unsigned int height,
+				std::vector<Wizard> wizards, std::vector<unsigned int> temperatures)
+				: width(width), height(height), wizards(wizards), temperatures(temperatures) { };
 		};
 	}
 }
